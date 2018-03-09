e7fa817b03d6f8c7a64af2df5ee4f48ebfba5dcd
PlanCZero <wut.w@excelbangkok.com>
2018-03-09 19:57:32 +0700

    Implement data model of project.

--- src/Core/Feature.cs
+++ src/Core/Feature.cs
@@ -1,14 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TimeTracker.Model.Common;
 
 namespace TimeTracker.Model
 {
-    public class Feature:Entity<int>
+    public class Feature : AuditableEntity<Guid>
     {
         public string Name { get; set; }
+        public List<TaskModel> Tasks { get; set; }
     }
 }
