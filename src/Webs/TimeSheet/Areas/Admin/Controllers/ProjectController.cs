d20d9af711ca1367417dea63ba4b994c338aba28
PlanCZero <wuttipat@outlook.com>
2018-03-16 00:20:31 +0700

    Add create/view project.

--- src/Webs/TimeSheet/Areas/Admin/Controllers/ProjectController.cs
+++ src/Webs/TimeSheet/Areas/Admin/Controllers/ProjectController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TimeTracker.Model;
 
 namespace TimeSheet.Areas.Admin.Controllers
 {
@@ -11,6 +12,18 @@ namespace TimeSheet.Areas.Admin.Controllers
         // GET: Admin/Project
         public ActionResult Index()
         {
+            return View();
+        }
+        public ActionResult Create() {
+            return View();
+        }
+        public ActionResult Details(int id) {
+            return View();
+        }
+        public ActionResult Edit(Project project) {
+            return View();
+        }
+        public ActionResult Delete(int id) {
             return View();
         }
     }
